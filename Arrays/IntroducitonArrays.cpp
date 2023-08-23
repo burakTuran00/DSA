defa98c8ceab94da5850a774f98d5599b22e69df
burakTuran00 <burakturan7410@gmail.com>
2023-08-23 21:38:40 +0300

    DSA

--- Arrays/IntroducitonArrays.cpp
+++ Arrays/IntroducitonArrays.cpp
@@ -2,5 +2,25 @@
 
 int main()
 {
-    std::cout << "Hello";
+    int marks[100];
+
+    int n;
+    std::cout << "Enter the number of students:";
+    std::cin >> n;
+
+    // Input
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> marks[i];
+    }
+
+    // Output
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << marks[i] << " ,";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
 }
---
+++ Arrays/PassByReference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void printArray(int arr[])
+{
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+
+    return 0;
+}

811e69d22e0ef5e9090398edddd34be134fc65c5
burakTuran00 <burakturan7410@gmail.com>
2023-08-23 21:48:12 +0300

    DSA

--- Arrays/PassByReference.cpp
+++ Arrays/PassByReference.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 
-void printArray(int arr[])
+void printArray(int *arr, int n)
 {
+    std::cout << "In funciton " << sizeof(arr) << std::endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
 }
 
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
+
+    std::cout << "In main " << sizeof(arr) << std::endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
+
+    printArray(arr, n);
 
     return 0;
 }
